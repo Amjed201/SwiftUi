afbe2281ff56dfee85cc8f876d71e2f2d5a5b164
amjed <amjed4ever@hotmail.com>
2023-08-25 09:31:32 +0300

    Initial Commit

---
+++ firstApp/firstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstAppApp.swift
+//  firstApp
+//
+//  Created by Amjed Mohamed Babiker on 25/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct firstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
