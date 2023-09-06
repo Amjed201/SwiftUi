6c93211f60b4f117e2632bf68a541b40b5a4f650
amjed <amjed4ever@hotmail.com>
2023-09-07 02:57:12 +0300

    Create Categories View , and Tab View for Home Screen

--- firstApp/Model/ModelData.swift
+++ firstApp/Model/ModelData.swift
@@ -9,8 +9,20 @@ import Foundation
 
 final class ModelData: ObservableObject {
    @Published var landmarks: [Landmark] = load("landmarkData.json")
+    
     //not published in hikes because it will never be modified
     var hikes: [Hike] = load("hikeData.json")
+    
+    var categories: [String: [Landmark]] {
+        Dictionary(
+            grouping: landmarks,
+            by: { $0.category.rawValue }
+        )
+    }
+    
+    var features: [Landmark] {
+          landmarks.filter { $0.isFeatured }
+      }
     
 
 }
